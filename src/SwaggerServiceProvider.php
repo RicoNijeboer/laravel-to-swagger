a86998eb13ec3f821209d6878f63ceb2e84fd4f4
Rico Nijeboer <r.nijeboer@code14.nl>
2021-05-14 13:08:00 +0200

    Register middleware aliases

--- src/SwaggerServiceProvider.php
+++ src/SwaggerServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace RicoNijeboer\Swagger;
 
+use Illuminate\Routing\Router;
+use RicoNijeboer\Swagger\Middleware\SwaggerReader;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -15,5 +17,19 @@ class SwaggerServiceProvider extends PackageServiceProvider
             ->hasConfigFile('swagger')
             ->hasMigration('create_swagger_batches_table')
             ->hasMigration('create_swagger_entries_table');
+
+        $this->registerMiddleware();
+    }
+
+    /**
+     * @return void
+     */
+    protected function registerMiddleware(): void
+    {
+        /** @var Router $router */
+        $router = $this->app['router'];
+
+        $router->aliasMiddleware('swagger', SwaggerReader::class);
+        $router->aliasMiddleware('openapi', SwaggerReader::class);
     }
 }
